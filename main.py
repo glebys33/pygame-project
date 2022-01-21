4ee17216844f7b903a2dca56b3ebdc4abd796ca6
sekkragen33 <98112010+sekkragen33@users.noreply.github.com>
2022-01-21 15:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -259,6 +259,31 @@ class ApplyButton(pygame.sprite.Sprite):
         self.rect.x = WIDTH * 0.8
         self.rect.y = HEIGHT * 0.915
         self.button = pygame.Rect(self.rect)
+        
+   class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
+        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.image = Surface((WIDTH, HEIGHT))
+        self.image.fill(Color(COLOR))
+        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
+
+    def update(self, left, right):
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+
+    def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
 def update_all_sprites():
