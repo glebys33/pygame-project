c36cf59a54b8a3f0c7b403602c174d45a2a1ae83
glebys33 <glebys.kiselev@gmail.com>
2022-02-24 12:36:24 +0300

    commit

--- main.py
+++ main.py
@@ -222,6 +222,10 @@ class CloseButton(pygame.sprite.Sprite):
     def mouseup(self):
         if self.button.collidepoint(mouse_pos) and background.scane == 'settings':
             background.scane = 'menu'
+            with open(os.path.join('data', 'settings.txt')) as file:
+                for row in file:
+                    if row.split()[0] == 'volume':
+                        pygame.mixer.music.set_volume(float(row.split()[1]))
 
     def update_sprite(self):
         CloseButton.image = pygame.transform.scale(load_image('close.png'), (int(WIDTH * 0.1), int(HEIGHT * 0.04)))
@@ -247,10 +251,14 @@ class ApplyButton(pygame.sprite.Sprite):
         if self.button.collidepoint(mouse_pos) and background.scane == 'settings':
             background.scane = 'menu'
             global screen, WIDTH, HEIGHT
-            WIDTH = 520
-            HEIGHT = 720
+            WIDTH = 620
+            HEIGHT = 520
             screen = pygame.display.set_mode((WIDTH, HEIGHT))
             update_all_sprites()
+            with open(os.path.join('data', 'settings.txt'), 'w') as file:
+                file.write(f"""WIDTH 960
+HEIGHT 720
+volume {volume}""")
 
     def update_sprite(self):
         ApplyButton.image = pygame.transform.scale(load_image('apply.png'), (int(WIDTH * 0.1), int(HEIGHT * 0.04)))
@@ -259,31 +267,62 @@ class ApplyButton(pygame.sprite.Sprite):
         self.rect.x = WIDTH * 0.8
         self.rect.y = HEIGHT * 0.915
         self.button = pygame.Rect(self.rect)
-        
-   class Player(sprite.Sprite):
-    def __init__(self, x, y):
-        sprite.Sprite.__init__(self)
+
+
+class Player(pygame.sprite.Sprite):
+    imager = pygame.transform.scale(load_image('right.png'), (int(WIDTH * 0.06), int(HEIGHT * 0.07)))
+    imagel = pygame.transform.scale(load_image('left.png'), (int(WIDTH * 0.06), int(HEIGHT * 0.07)))
+
+    def __init__(self, group, x, y):
+        super().__init__(group)
         self.xvel = 0  # скорость перемещения. 0 - стоять на месте
         self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
         self.startY = y
-        self.image = Surface((WIDTH, HEIGHT))
-        self.image.fill(Color(COLOR))
-        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
+        self.image = Player.imager
+        self.rect = pygame.Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
 
-    def update(self, left, right):
+    def move(self, left, right):
         if left:
-            self.xvel = -MOVE_SPEED  # Лево = x- n
-
+            self.image = Player.imagel
+            self.xvel = -10  # Лево = x- n
         if right:
-            self.xvel = MOVE_SPEED  # Право = x + n
-
+            self.image = Player.imager
+            self.xvel = 10  # Право = x + n
         if not (left or right):  # стоим, когда нет указаний идти
             self.xvel = 0
-
         self.rect.x += self.xvel  # переносим свои положение на xvel
 
     def draw(self, screen):  # Выводим себя на экран
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Slider():
+    def __init__(self, x, y, w, h, val, mval):
+        super().__init__()
+        self.mval = mval
+        self.val = val
+        self.circle_x = x + val / mval * w
+        self.rect = pygame.Rect(x, y, w, h)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (205, 205, 205), self.rect)
+        pygame.draw.circle(screen, (255, 255, 255), (self.circle_x, self.rect.h // 2 + self.rect.y), self.rect.h * 1.5)
+
+    def move_event(self, screen, x, y):
+        if x < self.rect.x and y in range(self.rect.y - self.rect.h * 3, self.rect.y + self.rect.h * 3):
+            self.circle_x = self.rect.x
+        elif x > self.rect.x + self.rect.w and y in range(self.rect.y - self.rect.h * 3, self.rect.y + self.rect.h * 3):
+            self.circle_x = self.rect.x + self.rect.w
+        elif y in range(self.rect.y - self.rect.h * 3, self.rect.y + self.rect.h * 3):
+            self.circle_x = x
+        self.draw(screen)
+
+    def get_val(self):
+        return (self.circle_x - self.rect.x) / self.rect.w * self.mval
+
+    def update_sprite(self, w, h):
+        self.rect = pygame.Rect(WIDTH // 8 * 5, HEIGHT // 6 * 1, WIDTH // 4, HEIGHT // 100)
+        self.circle_x = WIDTH // 8 * 5 + self.val / self.mval * WIDTH // 4
 
 
 def update_all_sprites():
@@ -295,6 +334,11 @@ def update_all_sprites():
     quit.update_sprite()
     apply.update_sprite()
     close.update_sprite()
+    slider1.update_sprite(WIDTH, HEIGHT)
+
+
+def settings_image():
+    screen.blit(pygame.transform.scale(load_image('voluem.png'), (int(WIDTH // 10 * 2), int(HEIGHT // 10 * 1))), (WIDTH // 9, HEIGHT // 9))
 
 
 if __name__ == '__main__':
@@ -311,10 +355,13 @@ if __name__ == '__main__':
     quit = QuitButton(menu_sprites)
     apply = ApplyButton(settings_sprites)
     close = CloseButton(settings_sprites)
+    slider1 = Slider(WIDTH // 8 * 5, HEIGHT // 6 * 1, WIDTH // 4, HEIGHT // 100, volume * 100, 100)
+    player = Player(game_sprites, WIDTH // 2, HEIGHT // 10 * 8.5)
     pygame.mouse.set_visible(False)
     pygame.mixer.music.play(-1)
     clock = pygame.time.Clock()
     running = True
+    move = (0, 0)
     while running:
         screen.fill('black')
         clock.tick()
@@ -324,6 +371,8 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEMOTION:
                 mouse.rect.x, mouse.rect.y = pygame.mouse.get_pos()
+                if background.scane == 'settings' and pygame.mouse.get_pressed(3)[0]:
+                    slider1.move_event(screen, mouse.rect.x, mouse.rect.y)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 start.mousedown()
@@ -338,6 +387,16 @@ if __name__ == '__main__':
                 running = quit.mouseup()
                 close.mouseup()
                 apply.mouseup()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    move = (0, 1)
+                if event.key == pygame.K_a:
+                    move = (1, 0)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    move = (0, 0)
+                if event.key == pygame.K_a:
+                    move = (0, 0)
         background_sprite.draw(screen)
         background_sprite.update()
 
@@ -347,6 +406,16 @@ if __name__ == '__main__':
         elif background.scane == 'settings':
             settings_sprites.draw(screen)
             settings_sprites.update()
+            slider1.draw(screen)
+            settings_image()
+        elif background.scane == 'game':
+            game_sprites.draw(screen)
+            game_sprites.update()
+            player.move(*move)
+
+        volume = int(slider1.get_val()) / 100
+        slider1.val = volume * 100
+        pygame.mixer.music.set_volume(volume)
 
         if pygame.mouse.get_focused():
             mouse_sprite.draw(screen)
